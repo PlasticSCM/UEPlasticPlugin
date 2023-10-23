e3c8d673b898d78dfd2e19161b6cfe7408006719
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2023-10-23 12:25:59 +0200

    POC add content to the new Tab Window: 3 faked branches
    TODO: like in the Source control history
    1. achieve a clear list or "table" look where the user can click on each row
    2. provide a context menu for actions

--- Source/PlasticSourceControl/Private/UnityVersionControlWindow.cpp
+++ Source/PlasticSourceControl/Private/UnityVersionControlWindow.cpp
@@ -17,7 +17,7 @@ void FUnityVersionControlWindow::Register()
 	FUnityVersionControlStyle::Initialize();
 	FUnityVersionControlStyle::ReloadTextures();
 
-	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(UnityVersionControlWindowTabName, FOnSpawnTab::CreateRaw(this, &FUnityVersionControlWindow::OnSpawnPluginTab))
+	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(UnityVersionControlWindowTabName, FOnSpawnTab::CreateRaw(this, &FUnityVersionControlWindow::OnSpawnTab))
 		.SetDisplayName(LOCTEXT("UnityVersionControlWindowTabTitle", "Unity Version Control"))
 		.SetMenuType(ETabSpawnerMenuType::Hidden)
 	.SetIcon(FSlateIcon(FUnityVersionControlStyle::Get().GetStyleSetName(), "UnityVersionControlWindow.PluginIcon.Small"));
@@ -30,31 +30,181 @@ void FUnityVersionControlWindow::Unregister()
 	FUnityVersionControlStyle::Shutdown();
 }
 
-TSharedRef<SDockTab> FUnityVersionControlWindow::OnSpawnPluginTab(const FSpawnTabArgs& SpawnTabArgs)
+TSharedPtr<SWidget> FUnityVersionControlWindow::CreateCacheStatisticsDialog()
 {
-	FText WidgetText = FText::Format(
-		LOCTEXT("WindowWidgetText", "Add code to {0} in {1} to override this window's contents"),
-		FText::FromString(TEXT("FUnityVersionControlWindow::OnSpawnPluginTab")),
-		FText::FromString(TEXT("UnityVersionControlWindow.cpp"))
-		);
+	return SNew(SDerivedDataCacheStatisticsDialog);
+}
 
+TSharedRef<SDockTab> FUnityVersionControlWindow::OnSpawnTab(const FSpawnTabArgs& SpawnTabArgs)
+{
 	return SNew(SDockTab)
 		.TabRole(ETabRole::NomadTab)
 		[
-			// Put your tab content here!
-			SNew(SBox)
-			.HAlign(HAlign_Center)
-			.VAlign(VAlign_Center)
-			[
-				SNew(STextBlock)
-				.Text(WidgetText)
-			]
+			CreateCacheStatisticsDialog().ToSharedRef()
 		];
 }
 
 void FUnityVersionControlWindow::OpenTab()
 {
 	FGlobalTabmanager::Get()->TryInvokeTab(UnityVersionControlWindowTabName);
+}
+
+
+void SDerivedDataCacheStatisticsDialog::Construct(const FArguments& InArgs)
+{
+	const float RowMargin = 0.0f;
+	const float TitleMargin = 10.0f;
+	const float ColumnMargin = 10.0f;
+	const FSlateColor TitleColor = FStyleColors::AccentWhite;
+	const FSlateFontInfo TitleFont = FCoreStyle::GetDefaultFontStyle("Bold", 10);
+
+	this->ChildSlot
+	[
+		SNew(SVerticalBox)
+		+ SVerticalBox::Slot()
+		.Padding(0, 20, 0, 0)
+		.AutoHeight()
+		[
+			SNew(SHorizontalBox)
+			+SHorizontalBox::Slot()
+			.FillWidth(1.0f)
+			[
+				SNew(STextBlock)
+				.Margin(TitleMargin)
+				.ColorAndOpacity(TitleColor)
+				.Font(TitleFont)
+				.Justification(ETextJustify::Left)
+				.Text(LOCTEXT("UnityVersionControl_Branches", "Branches"))
+			]
+		]
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		.Padding(0, 5, 0, 0)
+		.Expose(GridSlot)
+		[
+			GetGridPanel()
+		]
+	];
+
+	RegisterActiveTimer(0.5f, FWidgetActiveTimerDelegate::CreateSP(this, &SDerivedDataCacheStatisticsDialog::UpdateGridPanels));
+}
+
+EActiveTimerReturnType SDerivedDataCacheStatisticsDialog::UpdateGridPanels(double InCurrentTime, float InDeltaTime)
+{
+	(*GridSlot)
+	[
+		GetGridPanel()
+	];
+
+	SlatePrepass(GetPrepassLayoutScaleMultiplier());
+
+	return EActiveTimerReturnType::Continue;
+}
+
+
+TSharedRef<SWidget> SDerivedDataCacheStatisticsDialog::GetGridPanel()
+{
+	TSharedRef<SGridPanel> Panel =
+		SNew(SGridPanel);
+
+	const float RowMargin = 0.0f;
+	const float ColumnMargin = 10.0f;
+	const FSlateColor TitleColor = FStyleColors::AccentWhite;
+	const FSlateFontInfo TitleFont = FCoreStyle::GetDefaultFontStyle("Bold", 10);
+	const FMargin DefaultMarginFirstColumn(ColumnMargin, RowMargin);
+
+	int32 Row = 0;
+
+	const FMargin TitleMargin(0.0f, 10.0f, ColumnMargin, 10.0f);
+	const FMargin TitleMarginFirstColumn(ColumnMargin, 10.0f);
+	const FMargin DefaultMargin(0.0f, RowMargin, ColumnMargin, RowMargin);
+	
+	Panel->AddSlot(0, Row)
+	[
+		SNew(STextBlock)
+	];
+
+	Panel->AddSlot(1, Row)
+	[
+		SNew(STextBlock)
+		.Margin(TitleMarginFirstColumn)
+		.Font(FCoreStyle::GetDefaultFontStyle("Bold", 10))
+		.ColorAndOpacity(TitleColor)
+		.Text(LOCTEXT("BranchName", "Name"))
+	];
+
+	Panel->AddSlot(2, Row)
+	[
+		SNew(STextBlock)
+		.Margin(TitleMargin)
+		.ColorAndOpacity(TitleColor)
+		.Font(TitleFont)
+		.Text(LOCTEXT("CreateBy", "Created By"))
+	];
+
+	Panel->AddSlot(3, Row)
+	[
+		SNew(STextBlock)
+		.Margin(TitleMargin)
+		.ColorAndOpacity(TitleColor)
+		.Font(TitleFont)
+		.Text(LOCTEXT("CreationDate", "Creation date"))
+	];
+
+	Panel->AddSlot(4, Row)
+	[
+		SNew(STextBlock)
+		.Margin(TitleMargin)
+		.ColorAndOpacity(TitleColor)
+		.Font(TitleFont)
+		.Text(LOCTEXT("Comment", "Comment"))
+	];
+
+
+	// POC
+	for (int32 i = 0; i < 3; i++)
+	{
+		Row++;
+
+		Panel->AddSlot(0, Row)
+		[
+			SNew(STextBlock)
+		];
+
+		Panel->AddSlot(1, Row)
+		.HAlign(HAlign_Right)
+		[
+			SNew(STextBlock)
+			.Margin(DefaultMargin)
+			.Text(FText::FromString(TEXT("/main")))
+		];
+
+		Panel->AddSlot(2, Row)
+		.HAlign(HAlign_Right)
+		[
+			SNew(STextBlock)
+			.Margin(DefaultMargin)
+			.Text(FText::FromString(TEXT("sebastien.rombauts@unity3d.com")))
+		];
+
+		Panel->AddSlot(3, Row)
+		.HAlign(HAlign_Right)
+		[
+			SNew(STextBlock)
+			.Margin(DefaultMargin)
+			.Text(FText::FromString(TEXT("23/10/2023 14:24:14")))
+		];
+
+		Panel->AddSlot(4, Row)
+		.HAlign(HAlign_Right)
+		[
+			SNew(STextBlock)
+			.Margin(DefaultMargin)
+			.Text(FText::FromString(TEXT("Proof of concept branch")))
+		];
+	}
+
+	return Panel;
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/PlasticSourceControl/Private/UnityVersionControlWindow.h
+++ Source/PlasticSourceControl/Private/UnityVersionControlWindow.h
@@ -13,5 +13,26 @@ public:
 	void OpenTab();
 
 private:
-	TSharedRef<class SDockTab> OnSpawnPluginTab(const class FSpawnTabArgs& SpawnTabArgs);
+	TSharedRef<class SDockTab> OnSpawnTab(const class FSpawnTabArgs& SpawnTabArgs);
+
+	// TODO POC
+	TSharedPtr<SWidget> CreateCacheStatisticsDialog();
+};
+
+// TODO POC
+class SDerivedDataCacheStatisticsDialog : public SCompoundWidget
+{
+	SLATE_BEGIN_ARGS(SDerivedDataCacheStatisticsDialog) {}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs);
+
+private:
+
+	TSharedRef<SWidget> GetGridPanel();
+
+	// TODO: timer to replace by a binding with the search field (OnTextChanged)
+	EActiveTimerReturnType UpdateGridPanels(double InCurrentTime, float InDeltaTime);
+
+	SVerticalBox::FSlot* GridSlot = nullptr;
 };

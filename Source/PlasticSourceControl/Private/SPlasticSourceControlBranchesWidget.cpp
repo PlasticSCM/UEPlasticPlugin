2efee4f00d1b28c80bb31ee7c1b48b9427ceaf2f
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2024-02-21 19:17:55 +0100

    Upstream fix: Fixed Linux compile error in SPlasticSourceControlBranchesWidget.cpp: FPlasticSourceControlBranchPtr vs FPlasticSourceControlBranchRef usage in ::GetSelectedBranches().

--- Source/PlasticSourceControl/Private/SPlasticSourceControlBranchesWidget.cpp
+++ Source/PlasticSourceControl/Private/SPlasticSourceControlBranchesWidget.cpp
@@ -499,9 +499,9 @@ TArray<FString> SPlasticSourceControlBranchesWidget::GetSelectedBranches()
 {
 	TArray<FString> SelectedBranches;
 
-	for (const FPlasticSourceControlBranchPtr& BranchPtr : BranchesListView->GetSelectedItems())
+	for (const FPlasticSourceControlBranchRef& BranchRef : BranchesListView->GetSelectedItems())
 	{
-		SelectedBranches.Add(BranchPtr->Name);
+		SelectedBranches.Add(BranchRef->Name);
 	}
 
 	return SelectedBranches;

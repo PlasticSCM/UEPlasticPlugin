556be54519e1b1c986562264755b7ce97dae37cb
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2024-07-01 17:09:19 +0200

    Remove "EditorSytle" from dependencies since it's only used for versions previous than UE5.1

--- Source/PlasticSourceControl/PlasticSourceControl.Build.cs
+++ Source/PlasticSourceControl/PlasticSourceControl.Build.cs
@@ -6,12 +6,6 @@ public class PlasticSourceControl : ModuleRules
 {
 	public PlasticSourceControl(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		// Note: from UE5.2 onward, bEnforceIWYU = true; is replaced by IWYUSupport = IWYUSupport.Full;
-		bEnforceIWYU = true;
-		// IWYUSupport = IWYUSupport.Full;
-
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -21,7 +15,6 @@ public class PlasticSourceControl : ModuleRules
 				"Slate",
 				"SlateCore",
 				"InputCore",
-				"EditorStyle",
 				"UnrealEd",
 				"LevelEditor",
 				"DesktopPlatform",
@@ -36,7 +29,6 @@ public class PlasticSourceControl : ModuleRules
 			}
 		);
 
-		// NOTE: this produces warnings in Engine code in UE5.0
-		UnsafeTypeCastWarningLevel = WarningLevel.Warning;
+		UnsafeTypeCastWarningLevel = WarningLevel.Error;
 	}
 }

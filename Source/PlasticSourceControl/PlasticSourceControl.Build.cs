41105ae98d535ab978af2ca2d5047c33ead6e961
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2025-01-27 15:30:58 +0100

    Update CppCompileWarningSettings.UnsafeTypeCastWarningLevel = WarningLevel.Error;

--- Source/PlasticSourceControl/PlasticSourceControl.Build.cs
+++ Source/PlasticSourceControl/PlasticSourceControl.Build.cs
@@ -29,6 +29,6 @@ public class PlasticSourceControl : ModuleRules
 			}
 		);
 
-		UnsafeTypeCastWarningLevel = WarningLevel.Error;
+		CppCompileWarningSettings.UnsafeTypeCastWarningLevel = WarningLevel.Error;
 	}
 }

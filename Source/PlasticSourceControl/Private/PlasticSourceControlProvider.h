5567cf7eb9196500a976b0af5f42f4265c56e63d
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2025-11-21 18:35:24 +0100

    Fix from upstream UnrealEngine 5.7: default overrides of FPlasticSourceControlProvider::GetStateBranchAtIndex()
    Commit 6678228e by sebastien lussier, 08/15/2025 04:24 PM
    
    World Partition - HLOD: Added new option -ReuseParentBranchHLODs for HLOD builds
    * When used, this will force HLOD builds to retrieve the parent branch (SCC) HLOD actor file (OFPA), and test whether its HLOD hash matches what we are about to build
    * If it does, we will skip the HLOD build and use the parent branch HLOD file instead
    * This will help reduce the the impact on patch size of building HLODs in multiple branches
    
    [CL 44889206 by sebastien lussier in ue5-main branch]

--- Source/PlasticSourceControl/Private/PlasticSourceControlProvider.h
+++ Source/PlasticSourceControl/Private/PlasticSourceControlProvider.h
@@ -39,6 +39,7 @@ public:
 	virtual bool QueryStateBranchConfig(const FString& ConfigSrc, const FString& ConfigDest) override { return false; }
 	virtual void RegisterStateBranches(const TArray<FString>& BranchNames, const FString& ContentRoot) override {}
 	virtual int32 GetStateBranchIndex(const FString& InBranchName) const override { return INDEX_NONE; }
+	virtual bool GetStateBranchAtIndex(int32 BranchIndex, FString& OutBranchName) const /* override NOTE: added in UE5.7 */ { return false; }
 	virtual ECommandResult::Type GetState(const TArray<FString>& InFiles, TArray<FSourceControlStateRef>& OutState, EStateCacheUsage::Type InStateCacheUsage) override;
 #if ENGINE_MAJOR_VERSION == 5
 	virtual ECommandResult::Type GetState(const TArray<FSourceControlChangelistRef>& InChangelists, TArray<FSourceControlChangelistStateRef>& OutState, EStateCacheUsage::Type InStateCacheUsage) override;
